6b67e6ca2532df4f8672734f51d8d5e5213087c2
Anderson <anderson@gmail.com>
2019-05-29 03:07:26 -0300

    configurate admin screen part07

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
@@ -22,4 +25,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_texts
+        return self.choice_text
